a75cf84e77960c1258d7bfd7e01745408680bffc
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2017-08-06 18:41:46 +0200

    feat(ui): Added installation method for enhanced search results plugin
    Also added dependency for moving the sidebar to a slider

---
+++ .bin/installSearchResults.sh
@@ -0,0 +1,4 @@
+git clone -b master git@github.com:Spangenhelm/TW5-EnhancedSearchResults.git EnhancedSearchResults
+mkdir -p tw/plugins/spangenhelm
+mv EnhancedSearchResults/dist/* tw/plugins/spangenhelm/
+rm -rf EnhancedSearchResults
